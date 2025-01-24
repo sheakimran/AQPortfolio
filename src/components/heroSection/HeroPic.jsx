259c0db5c32683e113c4f3373a3725eb7052a2e4
sheakimran <imran.antor2010@gmail.com>
2025-01-24 13:00:31 +0600

    new file added1

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,8 +12,8 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../images/HexaPic.png"
-        alt="Abdelrahman Qassem"
+        src="../../images/imran.png"
+        alt="Sheak Imran"
         className="max-h-[450px] w-auto"
       />
 
